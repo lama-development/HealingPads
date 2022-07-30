ba8d20b3442b130aa2f525991dac1c92ae33f8fd
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-07-30 18:11:20 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,12 @@ description 'Add paid healing pads at hospitals.'
 author 'Lama#9612'
 version '1.0'
 
-client_script 'client.lua'
-server_scripts {
+client_script {
+    'client.lua',
+    'config.lua'
+}
+
+server_script {
     'server.lua',
     'versioncheck.lua'
 }
