f06c4598618100bd9ed0742cbe664039126d484f
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-05 12:59:05 +0200

    Re-added old version checker for now

---
+++ versioncheck.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread( function()
+	updatePath = "/ItzEndah/HealingPads"
+	resourceName = "HealingPads by Lama"
+	
+	function checkVersion(err, responseText, headers)
+		-- Returns the current version set in fxmanifest.lua
+		curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+
+		if curVersion ~= responseText then
+			print("^1" .. resourceName .. ": there was an error retrieving the latest version of the resource from GitHub. The version checker will be skipped.")	
+		else if tonumber(curVersion) < tonumber(responseText) then
+			print("^1" .. resourceName .. " is outdated.\nLatest version: " .. responseText .. "Current version: " .. curVersion .. "\nPlease update it from: https://github.com" .. updatePath)
+		else
+			print("^2" .. resourceName .." is up to date. Enjoy.")
+		end
+	end
+end
+	
+	PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/main/version", checkVersion, "GET")
+end)
