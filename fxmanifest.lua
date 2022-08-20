0a842407c263f26798aec481a15897be7248a41b
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 16:15:00 +0200

    Removed versioncheck.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,11 @@
 name 'HealingPads'
-description 'Heal yourself at hospitals.'
+description 'Heal yourself at hospitals'
 author 'Lama#9612'
 version '1.1'
 
 fx_version 'cerulean'
 game 'gta5'
 
-shared_script 'config.lua'
 client_script 'client.lua'
-server_scripts {
-  'server.lua',
-  'versioncheck.lua'
-}
+server_script 'server.lua'
+shared_script 'config.lua'
