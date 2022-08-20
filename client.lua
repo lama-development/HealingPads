f51d11525297ef8b91b3c5d696e66eb2b4e6c011
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 16:38:29 +0200

    Fixed blank blip names

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@
 HealingPads - Created by Lama	
 For support - Lama#9612 on Discord	
 Do not edit below if you don't know what you are doing
-]] --
+]]--
 
 -- Notification above map
 function DisplayNotification(text)
@@ -33,42 +33,33 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Get ped and distance from healing pad.
-CreateThread(function()
+Citizen.CreateThread(function()
     while true do
-        Wait(0)
+        Citizen.Wait(0)
         ped = PlayerPedId()
         for _, item in pairs(Config.Blips) do
+            -- Get distance from healing pad
             distance = #(GetEntityCoords(ped) - vector3(item.x, item.y, item.z))
-        end
-    end
-end)
-
--- See healing pads when near, and when inside a healing pad and E is pressed then heal and pay if using ND
-CreateThread(function()
-    while true do
-        Wait(0)
-        for _, item in pairs(Config.Blips) do
             if distance <= 15.0 then
                 -- 23 is the marker type, refer to this if you want to change it https://docs.fivem.net/docs/game-references/markers/
                 -- 248, 138, 138 are the RGB values that determines the color of the blip
-                DrawMarker(23, item.x, item.y, item.z, 0, 0, 0, 0, 0, 0, 1.75, 1.75, 1.0, 248, 138, 138, 100)
-            end
-            if distance <= 2.0 then
-                DisplayHelpText("Press ~INPUT_VEH_HORN~ to get treated by hospital staff", 0)
-                -- Default key is E (38). Refer to this if you want to change it https://docs.fivem.net/docs/game-references/controls/
-                if IsControlJustPressed(1, 38) then
-                    if GetEntityHealth(ped) < 200 then
-                        SetEntityHealth(ped, 200)
-                        if Config.UseND then
-                            local price = math.random(Config.PriceMin, Config.PriceMax)
-                            TriggerServerEvent('pay', price)
-                            DisplayNotification("~g~You have been succesfully treated.~s~ Price: $" .. price)
+                DrawMarker(23, item.x, item.y, item.z, 0, 0, 0, 0, 0, 0, 1.75, 1.75, 1.0, 248, 138, 138, 128)
+                if distance <= 2.0 then
+                    DisplayHelpText("Press ~INPUT_VEH_HORN~ to get treated by hospital staff", 0)
+                    -- Default key is E (38). Refer to this if you want to change it https://docs.fivem.net/docs/game-references/controls/
+                    if (IsControlJustPressed(1, 38)) then
+                        if (GetEntityHealth(ped) < 200) then                          
+                            SetEntityHealth(ped, 200)
+                            if Config.UseND == true then
+                                price = math.random(Config.PriceMin, Config.PriceMax)
+                                TriggerServerEvent('pay', price)
+                                DisplayNotification("~g~You have been succesfully treated.~s~ Price: $" .. price)
+                            else
+                                DisplayNotification("~g~You have been succesfully treated.")
+                            end
                         else
-                            DisplayNotification("~g~You have been succesfully treated.")
+                            DisplayNotification("~r~You don't need treatment.")
                         end
-                    else
-                        DisplayNotification("~r~You don't need treatment.")
                     end
                 end
             end
