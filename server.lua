92fa0b49249e7d8c17bd337fdae29bf2b734bb42
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 15:53:09 +0200

    moved version checker here

--- server.lua
+++ server.lua
@@ -1,5 +1,4 @@
 -- from https://ndframework.gitbook.io/nd-framework/developers/server
-
 if Config.UseND then
 	NDCore = exports["ND_Core"]:GetCoreObject()
 	RegisterServerEvent('pay')
@@ -8,3 +7,25 @@ if Config.UseND then
 		NDCore.Functions.DeductMoney(price, player, "bank")
 	end)
 end
+
+-- version checker
+Citizen.CreateThread( function()
+	updatePath = "/ItzEndah/HealingPads"
+	resourceName = "HealingPads by Lama"
+	
+	function checkVersion(err, responseText, headers)
+		-- Returns the version set in the file
+		curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+
+		if responseText == nil or curVersion == nil then
+			print("^1There was an error retrieving the version of " .. resourceName .. ": the version checker will be skipped.")
+		else if tonumber(curVersion) == tonumber(responseText) then
+			print("^2" .. resourceName .." is up to date. Enjoy.")
+		else 
+			print("^1" .. resourceName .. " is outdated.\nLatest version: " .. responseText .. "Current version: " .. curVersion .. "\nPlease update it from: https://github.com" .. updatePath)
+		end
+	end
+end
+	
+	PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/main/version", checkVersion, "GET")
+end)
