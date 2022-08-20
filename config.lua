5111ba260932583c56dfd79d2702b4292a1159da
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-08-20 15:45:06 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -16,13 +16,12 @@ Config.UseND = false
 -- sets the minimum and maximum price for healing
 -- only works if UseND is set to true
 Config.PriceMin = 50
-Config.PriceMax = 550
+Config.PriceMax = 200
 
 -- healing pads locations
 -- refer to this for the blips ids and colors https://docs.fivem.net/docs/game-references/blips/
 Config.Blips = {{
     -- Central Los Santos Medical Center
-    name = "Healing Pad",
     id = 153,
     x = 338.85,
     y = -1394.56,
@@ -30,7 +29,6 @@ Config.Blips = {{
     color = 35
 },{
     -- Mount Zonah Medical Center
-    name = "Healing Pad",
     id = 153,
     x = -449.67,
     y = -340.83,
@@ -38,7 +36,6 @@ Config.Blips = {{
     color = 35
 }, {
     -- Pillbox Hill Medical Center
-    name = "Healing Pad",
     id = 153,
     x = 356.70,
     y = -595.00,
@@ -46,7 +43,6 @@ Config.Blips = {{
     color = 35
 }, {
     -- Sandy Shores Medical Center
-    name = "Healing Pad",
     id = 153,
     x = 1839.32,
     y = 3673.26,
@@ -54,7 +50,6 @@ Config.Blips = {{
     color = 35
 }, {
     -- Paleto Bay Care Center
-    name = "Healing Pad",
     id = 153,
     x = -247.48,
     y = 6332.39,
